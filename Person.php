b0bb36a5afe050fcd1775821156d950ad344d68c
Yusuke Yamamoto <yusuke@mac.com>
2016-05-20 14:18:54 +0900

    initial commit

---
+++ Person.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: yusuke
+ * Date: 5/20/16
+ * Time: 14:12
+ */
+class Person
+{
+    protected $name;
+    protected $age;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param mixed $age
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+
+    /**
+     * Person constructor.
+     * @param $name
+     * @param $age
+     */
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function isYoung()
+    {
+        return $this->age < 18;
+    }
+
+
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yusuke
+ * Date: 5/20/16
+ * Time: 14:13
+ */
+$person = new Person("yusuke", 18);
